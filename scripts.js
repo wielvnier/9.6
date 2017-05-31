cbc322e7754ec43eb699fb3c202e2dd98ea56a5d
Michał Gacek <gacek.michael@outlook.com>
2017-05-31 16:38:23 +0200

    Fixes

--- scripts.js
+++ scripts.js
@@ -3,20 +3,22 @@ var newButtonElem = document.createElement('button');
 
 newUlElem.id += 'id-ul';
 newButtonElem.id += 'id-button';
+newButtonElem.innerText = "Dodaj";
 
 var body = document.getElementsByTagName('body');
 body[0].appendChild(newUlElem);
 body[0].appendChild(newButtonElem);
 
 var ulElem = document.getElementById('id-ul');
+var liElem = document.getElementsByTagName('li');
 var buttonElem = document.getElementById('id-button');
 
 function checkOnclickEvent(param) {
-  var newLiElem = document.createElement('li');
-	ulElem.appendChild(newLiElem);
-	newLiElem.innerHTML = 'item';
+    var newLiElem = document.createElement('li');	
+	newLiElem.innerHTML = 'item ' + liElem.length;  
+    ulElem.appendChild(newLiElem);
 }
 
-buttonEleme.addEventListener('click', function(e) {
-  checkOnclickEvent()
+buttonElem.addEventListener('click', function(e) {
+    checkOnclickEvent();
 });
